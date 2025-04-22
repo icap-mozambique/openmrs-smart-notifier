1fcbc39d8ad3e1d5ebf34c0025fbe91fef77e136
steliomo <steliom@gmail.com>
2025-04-22 15:01:21 +0200

    Refactoring

--- api/src/test/java/org/openmrs/module/smartnotifier/util/AbstractIntegrationTest.java
+++ api/src/test/java/org/openmrs/module/smartnotifier/util/AbstractIntegrationTest.java
@@ -17,41 +17,42 @@ import org.openmrs.test.TestUtil;
  * @author Stélio Moiane
  */
 public abstract class AbstractIntegrationTest extends BaseModuleContextSensitiveTest {
-	
+
 	@Override
 	public Boolean useInMemoryDatabase() {
 		final Properties properties = TestUtil.getRuntimeProperties(this.getWebappName());
-		
+
 		System.setProperty("databaseUrl", properties.getProperty("connection.url"));
 		System.setProperty("databaseUsername", properties.getProperty("connection.username"));
 		System.setProperty("databasePassword", properties.getProperty("connection.password"));
-		
+
 		System.setProperty("databaseDriver", "com.mysql.jdbc.Driver");
 		System.setProperty("databaseDialect", "org.hibernate.dialect.MySQLDialect");
-		
+
 		return false;
 	}
-	
+
 	protected abstract String username();
-	
+
 	protected abstract String password();
-	
+
+	@SuppressWarnings("deprecation")
 	@Before
 	public void initialize() throws ContextAuthenticationException {
 		Context.openSession();
 		Context.authenticate(this.username(), this.password());
 	}
-	
+
 	@After
 	public void close() {
 		Context.closeSession();
 	}
-	
+
 	@Override
 	public void deleteAllData() {
 		System.out.println("Do not delete DATA...");
 	}
-	
+
 	@Override
 	public void baseSetupWithStandardDataAndAuthentication() throws SQLException {
 		System.out.println("Do not add DATA...");
